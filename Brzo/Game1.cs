e86c51b5d7cba9aaadf80885ec40bfd315bd336b
Selectricity Productions <selectricityproductions@gmail.com>
2024-05-14 20:03:16 +0200

    Testing of Texture2D Drawing (_tl test 1)

--- Brzo/Game1.cs
+++ Brzo/Game1.cs
@@ -9,6 +9,10 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    //DELETE LATER
+    private Texture2D _tl;
+
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -26,7 +30,7 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        _tl = Content.Load<Texture2D>("tl_light");
         // TODO: use this.Content to load your game content here
     }
 
@@ -42,8 +46,13 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
+
+        
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(_tl, new Rectangle(400-(96/2), 240-(35/2), 96, 35), Color.White);
+        _spriteBatch.End();
         // TODO: Add your drawing code here
 
         base.Draw(gameTime);
